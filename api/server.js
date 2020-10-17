587fd6efcde1910283f44cc3c536649f56b111aa
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-16 23:41:07 -0700

    update server and knex

--- api/server.js
+++ api/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const morgan = require ("morgan")
+
+
 
 const server = express();
 
-server.use(helmet());
+server.use("/",helmet());
 server.use(express.json());
+server.use("/", morgan("---testing for creating API sprint---"));
 server.use(cors());
 
 
-server.get("/", (req, res) => {
-  res.json({ api: "Welcome to Virtual Reality Funding Platform" });
+server.get("/", (req, res, next) => {
+  res.json({ api: "Welcome to Virtual Reality Funding Platform", query: req.query  })  
 });
 
 module.exports = server;
