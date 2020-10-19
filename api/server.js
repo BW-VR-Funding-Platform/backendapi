af5e22e5bdd7a237de5cffc572df330b12eb6b13
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-19 14:11:23 -0700

    update server,js

--- api/server.js
+++ api/server.js
@@ -4,25 +4,29 @@ const helmet = require("helmet");
 const cors = require("cors");
 const morgan = require ("morgan");
 
+//router
 const authRouter = require("../auth/auth-router");
 const userRouter = require("../user/user-router");
 const projRouter = require("../router/projrouter");
 const restrictedMw = require("../auth/restricted-mw");
 
+//server
 const server = express();
 
-server.use("/",helmet());
+//middleware
 server.use(express.json());
+server.use("/",helmet());
 server.use("/", morgan("---testing for creating API sprint---"));
 server.use(cors());
 
+//routers
 server.use("/api/auth", authRouter);
 server.use("/api/users", userRouter);
 server.use("/api/projects",restrictedMw, projRouter);
 
 
 server.get("/", (req, res) => {
-  res.json({ api: "Welcome to Virtual Reality Funding Platform" })  
+  res.json({ api: "Hi!Welcome to Virtual Reality Funding Platform" })  
 });
 
 module.exports = server;
