3727ae08754b0bdac52fcbd29b80572b7ff81aa5
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-20 15:13:46 -0700

    debugged post request

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -10,13 +10,17 @@ function find() {
     .select("user_id", "firstname", "lastname")
     .orderBy("user_id");
 }
-async function add(users) {
-  try {
-    const [user_id] = await db("users").insert(firstname, lastname, "user_id");
-    return findById(user_id);
-  } catch (error) {
-    throw error;
-  }
+ function add(users) {
+    // const [user_id] = db("users").insert(firstname, lastname, "user_id");
+    // return findById(user_id)
+    return db("users")
+    .insert(user)
+    .then((id) => {
+      return db("users").where({ id : id[0]}).first();
+    })
+    .catch((err) => {
+      return err;
+    })
 }
 
 function findById(user_id) {
