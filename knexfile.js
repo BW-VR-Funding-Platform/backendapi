38bd2106599b81435f7595a968b5cdad8aaabd8a
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-20 14:41:19 -0700

    update knex to debug api link

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,11 @@
 // Update with your config settings.
 const knex = require("knex");
-const pgConnection =
-  process.env.DATABASE_URL ||
-  "postgres://xuhmadekezmvyo:5cb99aa9c662e2d719e4806b941190fb48162f155923004ea890559cf8ec2ff7@ec2-3-95-87-221.compute-1.amazonaws.com:5432/d19mvvefqhb5pg";
+// const pgConnection =
+//   process.env.DATABASE_URL ||
+//   "postgres://xuhmadekezmvyo:5cb99aa9c662e2d719e4806b941190fb48162f155923004ea890559cf8ec2ff7@ec2-3-95-87-221.compute-1.amazonaws.com:5432/d19mvvefqhb5pg";
 
 require("dotenv").config(); //access to .env variables
-module.exports = knex(pgConnection);
+// module.exports = knex(pgConnection);
 module.exports = {
   development: {
     client: "sqlite3",
