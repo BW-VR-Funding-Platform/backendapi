5c203f28f89bb4bce00a5d0eb7f300569e55ad79
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-21 16:48:30 -0700

    funding endpoints work

--- router/fundingrouter.js
+++ router/fundingrouter.js
@@ -1,10 +1,10 @@
-const funds = require("express").Router();
+const fundrouter = require("express").Router();
 
 const fundmodel = require("./fundingmodel");
 
 //GET all the funding informations
 
-funds.get("/", (res, req) => {
+fundrouter.get("/", (res, req) => {
   fundmodel
     .find("funding")
     .then((res) => {
@@ -15,42 +15,54 @@ funds.get("/", (res, req) => {
     });
 });
 
+//GET funding by id
+fundrouter.get("/:id", (req, res) => {
+  const id = req.params.id;
+  fundmodel
+    .findById(id, "funding")
+    .then((funding) =>
+      res.status(200).json({
+        message: "This is information of project with specific ID",
+        funding,
+      })
+    )
+    .catch((err) => res.status(500).json({ status: 500, err }));
+});
 
 // UPDATE project information
-// funds.put("/:id", (req, res) => {
-//     const { id } = req.params;
-//     const changes = req.body;
+fundrouter.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
   
-//     fundmodel
-//       .findById(id, "funding")
-//       .then((funding) => {
-//         if (funding) {
-//           fundmodel.update(changes, id).then((updated) => {
-//             res
-//               .status(201)
-//               .json({
-//                 success: "Project information is updated",
-//                 ...changes,
-               
-//                 UpdatingInformation: updated,
-//               });
-//           });
-//         } else {
-//           res
-//             .status(401)
-//             .json({
-//               message: `Could Not Find Object With ID: ${id}`,
-//               error: err,
-//             });
-//         }
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         res
-//           .status(500)
-//           .json({ message: "Failed To Update Object", errormessage: err });
-//       });
-//   });
+    fundmodel
+      .findById(id, "funding")
+      .then((funding) => {
+        if (funding) {
+          fundmodel.update(changes, id).then((updated) => {
+            res
+              .status(201)
+              .json({
+                success: "Project information is updated",
+                ...changes,               
+                UpdatingInformation: updated,
+              });
+          });
+        } else {
+          res
+            .status(401)
+            .json({
+              message: `Could Not Find Object With ID: ${id}`,
+              error: err,
+            });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ message: "Failed To Update Object", errormessage: err });
+      });
+  });
 
 
-module.exports = funds;
+module.exports = fundrouter;
