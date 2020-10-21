9ed18fda6535197219ee05b243cec212a5013e6e
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-20 18:01:41 -0700

    update

--- migrations/20201017114553_projects.js
+++ migrations/20201017114553_projects.js
@@ -2,19 +2,15 @@ exports.up = function (knex) {
   return knex.schema.createTable("projects", (tbl) => {
     tbl.increments("id"); // id
 
-    tbl.integer("project_id")
-      .unsigned()
-      .notNullable()
-    ;
-
     tbl.string("project_name", 100).notNullable();
 
     tbl.string("project_founder", 100).notNullable();
 
     tbl.text("project_description", 250).notNullable();
 
-    tbl.decimal("project_goal", 65, 2) // numbers are allow after decimal, max size 65
-    
+    tbl
+      .decimal("project_goal", 65, 2) // numbers are allow after decimal, max size 65
+
       .notNullable();
   });
 }; // close for `.up`
